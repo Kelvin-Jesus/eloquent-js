0b2f4f54cf022fa0062a1594cabbbe7bdb634af9
Kelvin <kj.theprog@gmail.com>
2022-03-29 07:38:50 -0300

    first commit

---
+++ cap02 - Estrutura de um programa/01- expressoes-e-afirmações.js
@@ -0,0 +1,4 @@
+1;
+"KJ";
+!false;
+// Exemplos de expressões mas que não afetam em nada a execução do programa
---
+++ cap02 - Estrutura de um programa/02- variaveis.js
@@ -0,0 +1,21 @@
+var caught = 5 * 5;
+// declaração de uma variável, forma de guarda um valor/estado atual.
+
+var ten = 10;
+console.log(ten * ten);
+// 100
+
+var mood = 'light';
+console.log(mood);
+// light
+
+mood = 'dark';
+console.log(mood);
+// dark
+
+// É possível alterar o valor de uma variável durante a execução do programa, basta utilizar o operador "=" e em seguida uma expressão.
+
+var semValor;
+console.log(semValor);
+// undefined
+// Quando uma var é definida sem valor, por padrão ela recebe undefined
